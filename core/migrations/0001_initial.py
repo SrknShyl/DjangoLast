b0b2121d23754150e918ab77358cdcb5dc6f5122
SrknShyl <srknshyl@icloud.coml>
2024-05-24 02:13:09 +0300

    feat : general settings model to admin

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='GeneralSettings',
+            name='GeneralSetting',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(blank=True, default='', max_length=254)),
